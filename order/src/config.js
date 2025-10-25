d4b1bb17de30ad740d8a6ad2cea8a35bd0ec6b97
dienthinh280504 <nguyendienthinh2805@gmail.com>
2025-10-25 15:36:34 +0700

    commit lan 1

--- order/src/config.js
+++ order/src/config.js
@@ -1,9 +1,8 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-    mongoURI: process.env.MONGODB_ORDER_URI || 'mongodb://localhost/orders',
-    rabbitMQURI: 'amqp://localhost',
-    rabbitMQQueue: 'orders',
-    port: 3002
+  mongoURI: process.env.MONGODB_ORDER_URI || "mongodb://localhost/orders",
+  rabbitMQURI: "amqp://rabbitmq:5672",
+  rabbitMQQueue: "orders",
+  port: 3002,
 };
-  
